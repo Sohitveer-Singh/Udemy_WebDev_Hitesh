6ccefb14153529d926b08083b94b1e05d52e9af2
Sohit <118461525+Sohitveer-Singh@users.noreply.github.com>
2025-03-29 19:30:26 +0530

    Array Challenge and Conditional Challenge

---
+++ Challenges/ConditionalChallenge.js
@@ -0,0 +1,19 @@
+// Checking if a number is greater than another number:
+
+let num1 = 5;
+let num2 = 8;
+
+// Checking if a string is equal to another string:
+
+let username = "chai";
+let anotherUsername = "chai";
+
+//Checking if a variable is a number or not:
+
+let score = "44";
+
+//Checking if a boolean value is true or false:
+
+let isTeaReady = false;
+
+// Checking if an array is empty or not:
