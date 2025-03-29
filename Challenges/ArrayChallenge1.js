32a363ca3a34ce6a020ea01d8036643efe74786c
Sohit <118461525+Sohitveer-Singh@users.noreply.github.com>
2025-03-29 21:20:00 +0530

    Completed Advanced ArrayChallenge

---
+++ Challenges/ArrayChallenge1.js
@@ -0,0 +1,212 @@
+/* 
+1. Write a `for` loop that loops through the array `["green tea", "black tea", "chai", "oolong tea"]` and stops the loop when it finds `"chai"`. 
+   Store all teas before `"chai"` in a new array named `selectedTeas`.
+*/
+
+let teas = ["green tea", "black tea", "chai", "oolong tea"];
+let selectedTeas = [];
+for (let i = 0; i < teas.length; i++) {
+   if (teas[i] === "chai" ) {
+      break
+   }
+   else{
+      selectedTeas.push(teas[i])
+   }
+}
+console.log(selectedTeas);
+
+
+/* 
+2. Write a `for` loop that loops through the array `["London", "New York", "Paris", "Berlin"]` and skips `"Paris"`. 
+   Store the other cities in a new array named `visitedCities`.
+*/
+
+let cities = ["London", "New York", "Paris", "Berlin"];
+let visitedCities = []
+
+for (let j = 0; j < cities.length; j++) {
+   if (cities[j]==="Paris") {
+      continue
+   }
+   else{
+      visitedCities.push(cities[j])
+   }
+}
+console.log(visitedCities)
+
+
+/* 
+3. Use a `for-of` loop to iterate through the array `[1, 2, 3, 4, 5]` and stop when the number `4` is found. 
+   Store the numbers before `4` in an array named `smallNumbers`.
+*/
+let numbers = [1, 2, 3, 4, 5];
+let smallNumbers = []
+
+for (const number of numbers) {
+   if (number === 4) {
+      break
+   }
+   else{
+      smallNumbers.push(number)
+   }
+}
+console.log(smallNumbers);
+
+
+/* 
+4. Use a `for-of` loop to iterate through the array `["chai", "green tea", "herbal tea", "black tea"]` and skip `"herbal tea"`. 
+   Store the other teas in an array named `preferredTeas`.
+*/
+
+let teaTypes = ["chai", "green tea", "herbal tea", "black tea"];
+let preferredTeas = []
+for (const tea of teaTypes) {
+   if (tea === "herbal tea") {
+      continue
+   }
+   else{
+      preferredTeas.push(tea)
+   }
+}
+console.log(preferredTeas);
+
+
+/* 
+5. Use a `for-in` loop to loop through an object containing city populations. 
+   Stop the loop when the population of `"Berlin"` is found and store all previous cities' populations in a new object named `cityPopulations`.
+
+   let citiesPopulation = {
+    "London": 8900000,
+    "New York": 8400000,
+    "Paris": 2200000,
+    "Berlin": 3500000
+};
+
+*/
+
+let citiesPopulation = {
+   "London": 8900000,
+   "New York": 8400000,
+   "Paris": 2200000,
+   "Berlin": 3500000
+};
+
+let cityPopulations = {}
+
+for (const cities in citiesPopulation) {
+   if (cities === "Berlin" ) {
+      break
+   }
+   else{
+      cityPopulations[cities] = citiesPopulation[cities];
+   }
+}
+console.log(cityPopulations);
+
+
+/* 
+6. Use a `for-in` loop to loop through an object containing city populations. 
+   Skip any city with a population below 3 million and store the rest in a new object named `largeCities`.
+
+let worldCities = {
+    "Sydney": 5000000,
+    "Tokyo": 9000000,
+    "Berlin": 3500000,
+    "Paris": 2200000
+};
+
+   */
+let worldCities = {
+   "Sydney": 5000000,
+   "Tokyo": 9000000,
+   "Berlin": 3500000,
+   "Paris": 2200000
+};
+
+let largeCities = {}
+
+for (const city in worldCities) {
+   if (worldCities[city] < 3000000 ) {
+      continue
+   }
+   else{
+      largeCities[city] = worldCities[city]
+   }
+}
+console.log(largeCities);
+
+
+/* 
+7. Write a `forEach` loop that iterates through the array `["earl grey", "green tea", "chai", "oolong tea"]`. 
+   Stop the loop when `"chai"` is found, and store all previous tea types in an array named `availableTeas`.
+*/
+
+let teaCollection = ["earl grey", "green tea", "chai", "oolong tea"];
+let availableTeas = [];
+
+teaCollection.forEach(tea => {
+   if (tea ===  "chai") {
+      return
+   }
+   else{
+      availableTeas.push(tea)
+   }
+});
+console.log(availableTeas);
+
+
+/* 
+8. Write a `forEach` loop that iterates through the array `["Berlin", "Tokyo", "Sydney", "Paris"]`. 
+   Skip `"Sydney"` and store the other cities in a new array named `traveledCities`.
+*/
+
+let myWorldCities = ["Berlin", "Tokyo", "Sydney", "Paris"];
+let traveledCities = []
+
+myWorldCities.forEach(city => {
+   if (city === "Sydney") {
+      return
+   }
+   else{
+      traveledCities.push(city)
+   }
+});
+console.log(traveledCities);
+
+
+/* 
+9. Write a `for` loop that iterates through the array `[2, 5, 7, 9]`. 
+   Skip the value `7` and multiply the rest by 2. Store the results in a new array named `doubledNumbers`.
+*/
+
+let myNumbers = [2, 5, 7, 9];
+let doubledNumbers = []
+
+for (let k = 0; k < myNumbers.length; k++) {
+   if (myNumbers[k]===7) {
+      continue
+   }
+   else{
+      doubledNumbers.push(myNumbers[k]*2)
+   }
+}
+console.log(doubledNumbers)
+
+/* 
+10. Use a `for-of` loop to iterate through the array `["chai", "green tea", "black tea", "jasmine tea", "herbal tea"]` 
+    and stop when the length of the current tea name is greater than 10. 
+    Store the teas iterated over in an array named `shortTeas`.
+*/
+
+let myTeas = ["chai", "green tea", "black tea", "jasmine tea", "herbal tea"];
+let shortTeas = []
+
+for (const tea of myTeas) {
+   if (tea.length > 10) {
+      break
+   }
+   else{
+      shortTeas.push(tea)
+   }
+}
+console.log(shortTeas);
--- Challenges/ConditionalChallenge.js
+++ Challenges/ConditionalChallenge.js
@@ -3,17 +3,47 @@
 let num1 = 5;
 let num2 = 8;
 
+if (num1 > num2) {
+  console.log(`${num1} is greater than ${num2} `);
+} else if (num1 < num2) {
+  console.log(`${num2} is greater than ${num1} `);
+} else {
+  console.log();
+  console.log(`${num1} is equal to ${num2} `);
+}
+
 // Checking if a string is equal to another string:
 
 let username = "chai";
 let anotherUsername = "chai";
 
+if (username === anotherUsername) {
+  console.log(`${username} and ${anotherUsername} are Strings`);
+}
+
 //Checking if a variable is a number or not:
 
 let score = "44";
+
+console.log(score instanceof Number);
 
 //Checking if a boolean value is true or false:
 
 let isTeaReady = false;
 
+if (isTeaReady) {
+  console.log(`True`);
+} else {
+  console.log(`False`);
+}
+
 // Checking if an array is empty or not:
+
+let EmptyArray = [];
+
+if (EmptyArray.length === 0 ) {
+  console.log(`Array is Empty`);
+}
+else{
+    console.log(`Array is not Empty`)
+}
